a155c97f846679de75ccfe69d86ab7484f1b530d
Kato-30 <trandinhan19112003@gmail.com>
2025-06-04 17:00:53 +0700

    update parallax eff

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -1,24 +1,27 @@
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-    public GameObject pipePrefab;  // Đường ống mẫu
+    public ObjectPool pipePool;
     public float spawnInterval = 1.5f;  // Khoảng thời gian giữa các lần spawn
     public float heightOffset = 1.7f;  // Độ lệch chiều cao của đường ống
     public Transform pipeSpawnPoint; // Điểm spawn đường ống
 
     public void Spawner()
     {
-        InvokeRepeating("SpawnPipe", 1.5f, spawnInterval);
+        StartCoroutine(SpawnPipe());
     }
 
-    void SpawnPipe()
+    private IEnumerator SpawnPipe()
     {
-        // GameObject pipe = pipePool.GetObject();
-        float randomHeight = Random.Range(-heightOffset, heightOffset);
-        Vector3 spawnPosition = new Vector3(pipeSpawnPoint.position.x, randomHeight, 0);
-        Instantiate(pipePrefab, spawnPosition, Quaternion.identity);
-        // pipe.GetComponent<Transform>().position = spawnPosition;
+        while (true)
+        {
+            GameObject pipe = pipePool.GetObject();
+            float randomHeight = Random.Range(-heightOffset, heightOffset);
+            Vector3 spawnPosition = new Vector3(pipeSpawnPoint.position.x, randomHeight, transform.position.z);
+            pipe.transform.SetPositionAndRotation(spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(spawnInterval);
+        }
     }
 }
